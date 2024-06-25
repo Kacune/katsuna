5a327875e43a03d58434dd9a09f54d313a38ba13
Kacu <kacuakerman@gmail.com>
2024-06-26 03:02:18 +0700

    Add User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,25 @@ import { GoogleStrategy } from './google.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: 'katsuna',
-      entities: [],
-      synchronize: false,
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [ join(__dirname + '/../**/*.entity{.ts,.js}')],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
     }),
     AuthModule,
     UserModule,
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column({length: 30})
+    firstName: string
+
+    @Column({length: 30})
+    lastName: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @Column()
+    isActive: Boolean
+}
